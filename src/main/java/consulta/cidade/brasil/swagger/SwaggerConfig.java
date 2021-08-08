b0be82c0168a5ab9165d100c6636116ff71d528e
marcelosoliveira <msbobsk8@gmail.com>
2021-08-07 22:22:47 -0300

    Foi adicionado a documentacao da API com o swagger

--- src/main/java/consulta/cidade/brasil/swagger/SwaggerConfig.java
+++ src/main/java/consulta/cidade/brasil/swagger/SwaggerConfig.java
@@ -1,24 +1,26 @@
 package consulta.cidade.brasil.swagger;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
   @Bean
-  public Docket greetingApi() {
+  public Docket apiDoc() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
         .apis(RequestHandlerSelectors.basePackage("consulta.cidade.brasil"))
@@ -33,9 +35,21 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         .title("Consulta de Cidades do Brasil")
         .description("API para consultas da distância entre cidades do Brasil")
         .version("1.0.0")
+        .contact(new Contact("Marcelo dos Santos de Oliveira",
+                "https://www.linkedin.com/in/marcelofullstack/",
+                "msbobsk8@gmail.com"))
         .license("Apache License Version 2.0")
         .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
         .build();
+  }
+  
+  @Override
+  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
   
 }
